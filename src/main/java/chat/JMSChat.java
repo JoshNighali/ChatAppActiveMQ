9885d2a6299b37c6154ae383461aeb2a3636f906
Josh W. Nzighali <79741804+JoshNighali@users.noreply.github.com>
2023-09-08 08:39:44 +0200

    Add files via upload

--- src/main/java/chat/JMSChat.java
+++ src/main/java/chat/JMSChat.java
@@ -1,5 +1,5 @@
 package chat;
-
+// importation des librairies
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,6 +14,7 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.StreamMessage;
 import javax.jms.TextMessage;
+import javax.swing.JOptionPane;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
@@ -30,6 +31,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -47,52 +49,83 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
+// la classe de notre application
 public class JMSChat extends Application {
-	
-	// Variables Globales de l'Application sont declarées ici !
-	private MessageProducer messageProducer; 
-	private Session session; //Utilise la même session pour les messages Consumer et Producer 
-	private String codeUser;
+	// css code
+	String style_label =
+	        "-fx-font-weight: bolder;"
+	        + "-fx-text-fill: #FFFFFF;";
 
+	String style_button =
+			"-fx-background-color: #1C8AE5;"
+			+ "-fx-text-fill: #FFFFFF;"
+			+ "-fx-border-color: #1C8AE5;"
+			+ " -fx-border-width: 0px;"
+			+ "-fx-background-radius:10px;";
+	
+	String style_button_close =
+			"-fx-background-color: #E5401C;"
+			+ "-fx-text-fill: #FFFFFF;"
+			+ "-fx-border-color: #E5401C;"
+			+ " -fx-border-width: 0px;"
+			+ "-fx-background-radius:20px;";
+	
+	String style_button_off =
+			"-fx-background-color: #E5401C;"
+			+ "-fx-text-fill: #FFFFFF;"
+			+ "-fx-border-color: #E5401C;"
+			+ "-fx-border-width: 0px;"
+			+ "-fx-background-radius:30px;"
+			+ "-fx-height:20px;";
+	
+	String style_hbox =
+			"-fx-border-color: #ff0000;"
+			+ " -fx-border-width: 2px;";
+	
+	String style_hbox2 =
+			"-fx-border-color: #ff0000;"
+			+ " -fx-border-width: 2px;"
+			+ "-fx-background-color: #3BA41C;"
+			+ "-fx-height:20px;";
+	
+	String style_connected =
+			"-fx-border-color: #1F630B;"
+			+ " -fx-border-width: 2px;"
+			+ "-fx-background-color: #1F630B;";
+
+	String style_stage =
+			"-fx-background-image: url('images/cute-kitten-2560x1440-12770.jpg');"
+			+ "-fx-background-repeat: no-repeat;"
+			+ " -fx-background-size: 500 500;"
+			+ "-fx-background-position: center center;";
+	
+	String style_inputs ="-fx-background-radius: 20px;";
+
+	String style_message ="-fx-text-fill: #03F687;";
+	
+	String style_vbox_account ="-fx-background-color:#000000;"
+								+ "-fx-height:200px;";
+	
+	String style_separator ="-fx-border-width:20px;";
+	
+	
+	
+	
+	// la methode principale
 	public static void main(String[] args) {
 		Application.launch(JMSChat.class);
 	}
 
+	// la methode pour demarrer notre interface
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		File file = new File("images/chat.png");
+		Image image = new Image(file.toURI().toString());
+		
+		primaryStage.setTitle("Soft Chat | Version 1.0");
+		primaryStage.getIcons().add(image);
 		
 		
-		
-		// css code
-				String style_label =
-				        "-fx-font-weight: bolder;"
-				        + "-fx-text-fill: #FFFFFF;";
-
-				String style_button =
-						"-fx-background-color: #0C7042;"
-						+ "-fx-text-fill: #FFFFFF;"
-						+ "-fx-border-color: #ff0000;"
-						+ " -fx-border-width: 0px;"
-						+ "-fx-background-radius:20px;";
-				
-				String style_hbox =
-						"-fx-border-color: #ff0000;"
-						+ " -fx-border-width: 2px;";
-
-				String style_stage =
-						"-fx-background-image: url('images/1080634_ONIORX0.jpg');"
-						+ "-fx-background-repeat: no-repeat;"
-						+ " -fx-background-size: 500 500;"
-						+ "-fx-background-position: center center;";
-				
-				String style_inputs ="-fx-background-radius: 20px;";
-
-				String style_message ="-fx-text-fill: #03F687;";
-				
-				String style_vbox_account ="-fx-background-color:#000000;"
-											+ "-fx-height:200px;";
-				
-		primaryStage.setTitle("JMS Chat Application");
 		BorderPane borderPane = new BorderPane();
 		HBox hBox1 = new HBox();
 		hBox1.setPadding(new Insets(12));
@@ -101,7 +134,7 @@ public class JMSChat extends Application {
 		
 		
 		
-		Label labelUser = new Label("USER CODE :");
+		Label labelUser = new Label("User ID :");
 		labelUser.setStyle(style_label);
 		
 		
@@ -129,7 +162,7 @@ public class JMSChat extends Application {
 		
 		
 		          
-		Label labelHost = new Label("IP SERVER :");
+		Label labelHost = new Label("IP Server :");
 		labelHost.setStyle(style_label);
 		
 		
@@ -151,24 +184,42 @@ public class JMSChat extends Application {
 		textFieldPort.setFocusTraversable(false);
 		textFieldPort.setPromptText("PORT");
 		
-		Button buttonConnecter = new Button("CONNECTION");
+		Button buttonConnecter = new Button("Connection");
+		Button buttonDeconnecter = new Button("Fermer");
+		Button buttonStatus = new Button();
+		
 		buttonConnecter.setStyle(style_button);
+		buttonDeconnecter.setStyle(style_button_close);
+		buttonStatus.setStyle(style_button_off);
 		
-		
+		// Use a separator to better organize the layout.
+	    Separator separator = new Separator();
+	    separator.setPrefWidth(260);
+	    separator.setStyle(style_separator);
+	    
+	    
 		hBox1.getChildren().add(labelUser);
 		hBox1.getChildren().add(textFieldCode);
 		hBox1.getChildren().add(labelHost);
 		hBox1.getChildren().add(textFieldHost);
 		hBox1.getChildren().add(labelPort);
 		hBox1.getChildren().add(textFieldPort);
+		
+		//hBox1.getChildren().add(buttonStatus);
+		
 		hBox1.getChildren().add(buttonConnecter);
+		hBox1.getChildren().add(buttonDeconnecter);
 		borderPane.setTop(hBox1);
 		
 		VBox vBox = new VBox();
 		GridPane gridPane = new GridPane();
 		HBox hBox2 = new HBox();
+		vBox.getChildren().addAll(separator);
 		vBox.getChildren().add(gridPane);
 		vBox.getChildren().add(hBox2);
+		
+//		hBox2.setStyle(style_hbox2);
+		
 		vBox.setStyle("-fx-background-color : #000000;");
 		borderPane.setCenter(vBox);
 		
@@ -177,144 +228,52 @@ public class JMSChat extends Application {
 		TextField textFieldTo = new TextField("USER_");
 		textFieldTo.setStyle(style_inputs);
 		textFieldTo.setPrefWidth(300);
-		          
-		Label labelMessage = new Label("Message :");
-	          labelMessage.setFont(Font.font(STYLESHEET_MODENA, FontWeight.BOLD, FontPosture.ITALIC, 15));
-		TextArea textAreaMessage = new TextArea();
-		         textAreaMessage.setPrefWidth(300);
-		         textAreaMessage.setPrefRowCount(3);
-		Button buttonSendMessage = new Button("Send Text Message");
-	           buttonSendMessage.setStyle(style_button);
-
-		Label labelImage = new Label("Image :");
-              labelImage.setFont(Font.font(STYLESHEET_MODENA, FontWeight.BOLD, FontPosture.ITALIC, 15));
-		File f1=new File("images"); //Charger le contenu du dossier images
 		
-		ObservableList<String> observableListImages  = FXCollections.observableArrayList(f1.list());
-		ComboBox<String> comboBoxImages = new ComboBox<String>(observableListImages);
-		comboBoxImages.getSelectionModel().select(7); //la première image de la liste
-		Button buttonSendImage = new Button("Send Image File");
-               buttonSendImage.setFont(Font.font(STYLESHEET_MODENA, FontWeight.BLACK, FontPosture.REGULAR, 12));
-
-		gridPane.setPadding(new Insets(12));
-		gridPane.setVgap(12); gridPane.setHgap(12);
-		
-		gridPane.add(labelTo, 0, 0); gridPane.add(textFieldTo, 1, 0);
-		gridPane.add(labelMessage, 0, 1); 
-		gridPane.add(textAreaMessage, 1, 1);
-		gridPane.add(buttonSendMessage, 2, 1); 
-		gridPane.add(labelImage, 0, 2);
-		gridPane.add(comboBoxImages, 1, 2); 
-		gridPane.add(buttonSendImage, 2, 2);
-		
-		ObservableList<String> observableListMessages  = FXCollections.observableArrayList();
-		ListView<String> listViewMessages = new ListView<>(observableListMessages);
-		                 listViewMessages.setPrefWidth(700); listViewMessages.setPrefHeight(300);
-		 //Charger le fichier image choisie par défaut
-		File f2=new File("images/"+comboBoxImages.getSelectionModel().getSelectedItem());
-		Image image = new Image(f2.toURI().toString()); //Obtenir l'URI de l'image choisie par défaut
-		ImageView imageView = new ImageView(image);
-		          imageView.setFitWidth(400); imageView.setFitHeight(256);
-		hBox2.getChildren().add(listViewMessages);
-		hBox2.getChildren().add(imageView);
-		hBox2.setPadding(new Insets(15));
-		hBox2.setSpacing(15);
-
 		Scene scene = new Scene(borderPane, 1000, 500);
 		//scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-		//Sélectioner l'image et l'afficher dans la zone imageView
-		comboBoxImages.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-			
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				//Charger le fichier image sélectionée
-				File f3=new File("images/"+newValue);
-				Image image = new Image(f3.toURI().toString()); //Obtenir l'URI de l'image sélectionée
-				imageView.setImage(image);
-			}
-		});
 		
-		//Récupérer le message saisi et l'envoyer au destinataire
-		buttonSendMessage.setOnAction(e->{
-			try {
-				TextMessage textMessage = session.createTextMessage();
-				textMessage.setText(textAreaMessage.getText());
-				//textMessage.setStringProperty("user", codeUser);
-				textMessage.setStringProperty("user", textFieldTo.getText());
-				messageProducer.send(textMessage);
-			} catch (JMSException e1) {
-				e1.printStackTrace();
-			}
-		});
 		
-		//Envoi d'un fichier image à sélectionner dans la liste!
-		buttonSendImage.setOnAction(e->{
-			try {
-				StreamMessage streamMessage = session.createStreamMessage();
-				streamMessage.setStringProperty("user", textFieldTo.getText());
-				File f4=new File("images/"+comboBoxImages.getSelectionModel().getSelectedItem());
-				FileInputStream fis = new FileInputStream(f4);
-				byte[] data = new byte[(int)f4.length()];
-				fis.read(data); //lire les données images en octets
-				streamMessage.writeString(comboBoxImages.getSelectionModel().getSelectedItem());
-				streamMessage.writeInt(data.length);
-				streamMessage.writeBytes(data);
-				messageProducer.send(streamMessage);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-		});
-		
-		buttonConnecter.setOnAction(new EventHandler<ActionEvent>() {
+buttonConnecter.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
 			public void handle(ActionEvent event) {				
 				try {
-					codeUser = textFieldCode.getText();	
 					String host = textFieldHost.getText();	
 					int port = Integer.parseInt(textFieldPort.getText());
 					ConnectionFactory connectionFactory = new 
 							ActiveMQConnectionFactory("tcp://"+host+":"+port);
 				   Connection connection = connectionFactory.createConnection();
 				   connection.start();
-				   session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-				   Destination destination = session.createTopic("biu.chat");
-				   //MessageConsumer messageConsumer = session.createConsumer(destination);
-				   MessageConsumer messageConsumer = session.createConsumer(destination, "user='"+codeUser+"'");
-				   messageProducer = session.createProducer(destination); //Envoi et écoute sur la même file d'attente
-				   messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT); //messages non persistants au broker
 				   
-				   messageConsumer.setMessageListener(message->{
-					 try {
-						  if(message instanceof TextMessage) {
-							TextMessage textMessage = (TextMessage)message;
-							//System.out.println(textMessage.getText());
-							observableListMessages.add(textMessage.getText());
-						  }
-						  else if(message instanceof StreamMessage) {
-							 //Lire l'image envoyée
-							  StreamMessage streamMessage = (StreamMessage)message;
-							  String imageName = streamMessage.readString();
-							  observableListMessages.add("Réception d'une image : "+imageName);
-							  int size = streamMessage.readInt();
-							  byte[] data = new byte[size];
-							  streamMessage.readBytes(data);
-							  ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
-							  Image image = new Image(byteArrayInputStream);
-							  imageView.setImage(image);
-						  }
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				   });
-			    hBox1.setDisable(true);  //On désactive le hBox1 après avoir connecté un USER
+				   String message_connection = "Your User ID is : "+username+". Please, don't forget it!!";
+
+			    //hBox1.setDisable(true);  //On désactive le hBox1 après avoir connecté un USER
+				  JOptionPane.showMessageDialog(null, message_connection);
+				  hBox1.setStyle(style_connected);
+				  buttonConnecter.setVisible(false);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+
+buttonDeconnecter.setOnAction(new EventHandler<ActionEvent>() {
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		System.exit(0);
+		
 	}
+	
+});
+		
+	}
+	
+	
+	
+	
 }
